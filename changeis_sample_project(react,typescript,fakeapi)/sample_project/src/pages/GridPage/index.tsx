fd3bd9aa69e45aeca2241985390931bfd052090f
insonia78 <thomas.zangari.work@gtmail.com>
2024-09-23 09:37:37 -0400

    added loading

--- changeis_sample_project(react,typescript,fakeapi)/sample_project/src/pages/GridPage/index.tsx
+++ changeis_sample_project(react,typescript,fakeapi)/sample_project/src/pages/GridPage/index.tsx
@@ -8,32 +8,38 @@ import { PaginationComponent } from 'components/global/PaginationComponent';
 
 
 export const GridPage = () => {
-   
-   const [state, dispatch]: any = useReducer(fakeApiReducer,initialState)
-   const [ pagination, setPagination ] = useState({
-      paginationStart:0,
-      paginationEnd:5
+
+   const [state, dispatch]: any = useReducer(fakeApiReducer, initialState)
+   const [pagination, setPagination] = useState({
+      paginationStart: 0,
+      paginationEnd: 5
    })
-   const { payload , loading } = state
+   const { payload, loading } = state
    useEffect(() => {
       getDataFromFakerApi().
-         then((_data: any) => dispatch({ type: 'first_call_to_fakeApi', payload:{data:_data}}))
+         then((_data: any) => dispatch({ type: 'first_call_to_fakeApi', payload: { data: _data } }))
    }, [])
-                                           
-      
+
+
    return (
       <div
-       style={
-         {
-         display:"flex",
-         flexDirection:"column",
-         alignItems:"center",
-         
-       }}
+         style={
+            {
+               display: "flex",
+               flexDirection: "column",
+               alignItems: "center",
+
+            }}
       >
-         <PaginationComponent data={payload} pagination={pagination} setPagination={setPagination}/>
-         <CardComponent data={payload} pagination={pagination}/>
-         <PaginationComponent data={payload} pagination={pagination} setPagination={setPagination} />   
+         {loading ? "Loading..." :
+            (
+               <>
+                  <PaginationComponent data={payload} pagination={pagination} setPagination={setPagination} />
+                  <CardComponent data={payload} pagination={pagination} />
+                  <PaginationComponent data={payload} pagination={pagination} setPagination={setPagination} />
+               </>
+            )
+         }
       </div>
    );
 
